db8ef6e36a6559220ee65e34446807670747675c
sadun-sof <sadun.se97@gmail.com>
2023-01-23 18:15:07 +0530

    config-change

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const MyApiClient = axios.create({
+const MyApiClient = axios.create({
   baseURL: 'https://ttcuser-api.onrender.com',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
+
+export default MyApiClient
