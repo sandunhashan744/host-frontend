d8cd5ca6319660a5ddb85a504e4ddc82eb35610a
sadun-sof <sadun.se97@gmail.com>
2023-01-23 19:33:27 +0530

    dep

---
+++ src/helper/axios-url.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+const instance = axios.create({baseURL: 'https://ttcuser-api.onrender.com'});
+export default instance
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import axiosInstance from './axios-url'
 
-axios.defaults.baseURL = 'https://ttcuser-api.onrender.com'
+//axios.defaults.baseURL = 'https://ttcuser-api.onrender.com'
 //baseURL: process.env.REACT_APP_API_URL;
 
 // *** Make a API Request ***
@@ -64,7 +65,7 @@ export async function registerUser(credentials){
 export async function login({email, password}){
     try {
         if(email){
-            const { data } = await axios.post('/api/login', {email, password});
+            const { data } = await axiosInstance.post('/api/login', {email, password});
             return Promise.resolve({ data });
         }
     } catch (error) {
